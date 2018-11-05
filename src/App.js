66e01625be721fd9f26172247133dba763a98862
Daniel <dtkahn@gmail.com>
2018-11-05 15:34:14 -0500

    Setting initial state for the game in AppJS

--- src/App.js
+++ src/App.js
@@ -16,39 +16,46 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      db: []
+      // db: [],
+      unansweredQuestions: [],
+      answeredQuestions: [],
+      currentQuestion: [],
+      score: 0,
+      user: {},
+      timeRemaining: 60,
+      topic: ''
     };
   }
 
   componentDidMount() {
-    
-    // Get all docs in the users collection
-    dbUsers
-    .get()
-    .then(snapshot => {
-      snapshot.forEach( docRef => {
-        console.log(docRef.id, '=>', docRef.data());
-      })
-    });
+    // COMMENTING OUT ALL DB CODE WHILE GETTING THE GAME SET UP
+    // // Get all docs in the users collection
+    // dbUsers
+    // .get()
+    // .then(snapshot => {
+    //   snapshot.forEach( docRef => {
+    //     console.log(docRef.id, '=>', docRef.data());
+    //   })
+    // });
 
-    // Add new user to Firestore
-    dbUsers
-    .add({
-      email: 'kai@gmail.com',
-      fName: 'Kai',
-      lName: 'Berger-Medina'
-    })
-    .then(docRef => {
+    // // Add new user to Firestore
+    // dbUsers
+    // .add({
+    //   email: 'kai@gmail.com',
+    //   fName: 'Kai',
+    //   lName: 'Berger-Medina'
+    // })
+    // .then(docRef => {
       
-      dbUsers
-      .doc(docRef.id)
-      .update({ UID: docRef.id });
+    //   dbUsers
+    //   .doc(docRef.id)
+    //   .update({ UID: docRef.id });
       
-      console.log(docRef.id);
-    })
-    .catch( error => {
-      console.log(`Error writing to DB: ${error}`);
-    });
+    //   console.log(docRef.id);
+    // })
+    // .catch( error => {
+    //   console.log(`Error writing to DB: ${error}`);
+    // });
   }
 
   render() {
